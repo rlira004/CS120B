a26c0d076c9a629765ed1f06a0d18e3acb16cae6
rlira004 <rlira004@ucr.edu>
2020-05-03 14:08:55 -0700

    test

--- lab5/source/main.c
+++ lab5/source/main.c
@@ -1,148 +1,44 @@
-/*	Author: Ricardo Lira
+/*	Author: Ricardo Lira rlira004@ucr.edu
  *  Partner(s) Name: 
  *	Lab Section: 025
- *	Assignment: Lab #5  Exercise #2
+ *	Assignment: Lab #3  Exercise #2
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
  */
 #include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
 
-enum States {init, Start, Inc, Inc2, Dec, Dec2, Inc_rel, Dec_rel, Both, Both_rel} States;
-
-unsigned char tempA = 0x00;
-
-void tick(){
-	tempA = ~PINA;
-	switch (States)
-	{
-		case Start:
-		States = init;
-		break;
-		
-		case init:
-		if ((tempA & 0x03) == 0x01)
-		{
-			States = Inc;
-			}else if ((tempA & 0x03) == 0x02){
-			States = Dec;
-			}else{
-			States = init;
-		}
-		break;
-		case Inc:
-		if((tempA & 0x03) == 0x01){
-			States = Inc2;
-		}
-		break;
-		case Dec:
-		if((tempA & 0x03) == 0x02){
-			States = Dec2;
-		}
-		break;
-		case Inc2:
-		if((tempA & 0x03) == 0x01){
-			States = Inc2;
-			}else if ((tempA & 0x03) == 0x00){
-			States = Inc_rel;
-			}else if((tempA & 0x03) == 0x03){
-			States = Both;
-		}
-		break;
-		case Dec2:
-		if((tempA & 0x03) == 0x02){
-			States = Dec2;
-			}else if ((tempA & 0x03) == 0x00){
-			States = Dec_rel;
-			}else if((tempA & 0x03) == 0x03){
-			States = Both;
-		}
-		break;
-		case Inc_rel:
-		if ((tempA & 0x03) == 0x00)
-		{
-			States = Inc_rel;
-			}else if ((tempA & 0x03) == 0x01){
-			States = Inc;
-			}else if ((tempA & 0x03) == 0x02){
-			States = Dec;
-		}
-		break;
-		case Dec_rel:
-		if ((tempA & 0x03) == 0x00)
-		{
-			States = Dec_rel;
-			}else if ((tempA & 0x03) == 0x02){
-			States = Dec;
-			}else if ((tempA & 0x03) == 0x01){
-			States = Inc;
-		}
-		break;
-		case Both:
-		if ((tempA & 0x03) == 0x11)
-		{
-			States = Both;
-			}else if ((tempA & 0x03) == 0x01){
-			States = Both;
-			}else if ((tempA & 0x03) == 0x10){
-			States = Both;
-			}else if ((tempA & 0x03) == 0x00){
-			States = Both_rel;
-		}
-		break;
-		case Both_rel:
-		States = init;
-		break;
-		
-		default:
-		break;
-		
-		
+int main(void) {
+    /* Insert DDR and PORT initializations */
+    DDRA = 0x00; PORTA = 0xFF; 
+    DDRC = 0xFF; PORTC = 0x00;
+    unsigned char tmpA = 0x00;
+    /* Insert your solution below */
+    while (1) {
+	PORTC = 0x00;
+	tmpA = ~PINA;
+	if(tmpA == 0x01 || tmpA == 0x02) {
+		PORTC = 0x60;
 	}
-	
-	switch(States) {   // State actions
-		case init:
-		PORTC = 0x00;
-		break;
-		case Inc:
-		if(PINC != 0x09){
-			PORTC = PINC + 0x01;
-		}
-		break;
-		case Dec:
-		if(PINC != 0x00){
-			PORTC = PINC - 0x01;
-		}
-		break;
-		case Inc2:
-		break;
-		case Dec2:
-		break;
-		case Inc_rel:
-		break;
-		case Dec_rel:
-		break;
-		case Both:
-		break;
-		case Both_rel:
-		PORTC = 0x00;
-		break;
-		default:
-		break;
-	} // State actions
-	
-}
-
-
-int main(void)
-{
-	DDRA = 0x00; PORTA = 0xFF;
-	DDRC = 0xFF; PORTC = 0x00;
-	States = Start;
-	
-	while (1)
-	{
-		tick();
-	}
+	if(tmpA > 0x02 && tmpA < 0x05) {
+                PORTC = 0x70;
+        }
+	if(tmpA > 0x04 && tmpA < 0x07) {
+                PORTC = 0x38;
+        }
+	if(tmpA > 0x06 && tmpA < 0x0A) {
+                PORTC = 0x3C;
+        }
+	if(tmpA > 0x09 && tmpA < 0x0D) {
+                PORTC = 0x3E;
+        }
+	if(tmpA > 0x0C && tmpA < 0x10) {
+                PORTC = 0x3F;
+        }	
+    }
+	return 0;
 }
