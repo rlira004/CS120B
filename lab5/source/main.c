ba37038f0fa7415a3f303968b7c7c7b55776f3a0
Ricardo <63069526+rlira004@users.noreply.github.com>
2020-05-03 21:06:05 -0700

    Update main.c

--- lab5/source/main.c
+++ lab5/source/main.c
@@ -1,26 +1,26 @@
 /*	Author: Ricardo Lira rlira004@ucr.edu
  *  Partner(s) Name:
  *	Lab Section: 025
- *	Assignment: Lab#04  Exercise#02
+ *	Assignment: Lab#05  Exercise#03
  *	Exercise Description: [optional - include for your own benefit]
- *
+ *      youtube.com/watch?v=
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
  */
 #include <avr/io.h>
 
-enum States {Start, Wait, Inc, Keep_Inc, Fin_Inc, Dec, Keep_Dec, Fin_Dec, Reset, Fin_Reset} States;
+enum States { Start, Wait, Inc, Keep_Inc, Fin_Inc, Dec, Keep_Dec, Fin_Dec, Reset, Fin_Reset } States;
 unsigned char tempA = 0x00;
 
-void tick(){
+void tick() {
 	tempA = ~PINA;
 	switch (States)
 	{
-		case Start:
+	case Start:
 		States = Wait;
 		break;
-		
-		case Wait:
+
+	case Wait:
 		if ((tempA & 0x03) == 0x01)
 			States = Inc;
 		else if ((tempA & 0x03) == 0x02)
@@ -29,44 +29,44 @@ void tick(){
 			States = Wait;
 		break;
 
-		case Inc:
-		if((tempA & 0x03) == 0x01)
+	case Inc:
+		if ((tempA & 0x03) == 0x01)
 			States = Keep_Inc;
 		break;
 
-		case Keep_Inc:
-			if ((tempA & 0x03) == 0x01)
-				States = Keep_Inc;
-			else if ((tempA & 0x03) == 0x00)
-				States = Fin_Inc;
-			else if ((tempA & 0x03) == 0x03)
-				States = Reset;
-			break;
-
-		case Fin_Inc:
-			if ((tempA & 0x03) == 0x00)
-				States = Fin_Inc;
-			else if ((tempA & 0x03) == 0x01)
-				States = Inc;
-			else if ((tempA & 0x03) == 0x02)
-				States = Dec;
-			break;
-
-		case Dec:
-			if ((tempA & 0x03) == 0x02)
-				States = Keep_Dec;
-			break;
-
-		case Keep_Dec:
-		if((tempA & 0x03) == 0x02)
-			States = Keep_Dec;
+	case Keep_Inc:
+		if ((tempA & 0x03) == 0x01)
+			States = Keep_Inc;
 		else if ((tempA & 0x03) == 0x00)
-			States = Fin_Dec;
-		else if((tempA & 0x03) == 0x03)
+			States = Fin_Inc;
+		else if ((tempA & 0x03) == 0x03)
 			States = Reset;
 		break;
 
-		case Fin_Dec:
+	case Fin_Inc:
+		if ((tempA & 0x03) == 0x00)
+			States = Fin_Inc;
+		else if ((tempA & 0x03) == 0x01)
+			States = Inc;
+		else if ((tempA & 0x03) == 0x02)
+			States = Dec;
+		break;
+
+	case Dec:
+		if ((tempA & 0x03) == 0x02)
+			States = Keep_Dec;
+		break;
+
+	case Keep_Dec:
+		if ((tempA & 0x03) == 0x02)
+			States = Keep_Dec;
+		else if ((tempA & 0x03) == 0x00)
+			States = Fin_Dec;
+		else if ((tempA & 0x03) == 0x03)
+			States = Reset;
+		break;
+
+	case Fin_Dec:
 		if ((tempA & 0x03) == 0x00)
 			States = Fin_Dec;
 		else if ((tempA & 0x03) == 0x02)
@@ -75,7 +75,7 @@ void tick(){
 			States = Inc;
 		break;
 
-		case Reset:
+	case Reset:
 		if ((tempA & 0x03) == 0x11)
 			States = Reset;
 		else if ((tempA & 0x03) == 0x01)
@@ -86,54 +86,54 @@ void tick(){
 			States = Fin_Reset;
 		break;
 
-		case Fin_Reset:
+	case Fin_Reset:
 		States = Wait;
 		break;
-		
-		default:
+
+	default:
 		break;
-		
-		
+
+
 	}
-	
-	switch(States) {   // State actions
-		case Wait:
-		PORTC = 0x00;
+
+	switch (States) {   // State actions
+	case Wait:
+		PORTC = 0x15;
 		break;
 
-		case Inc:
-		if(PINC != 0x09)
-			PORTC = PINC + 0x01;
+	case Inc:
+		if (PINC != 0x09)
+			PORTC = 0x2A;
 		break;
 
-		case Dec:
-		if(PINC != 0x00)
+	case Dec:
+		if (PINC != 0x00)
 			PORTC = PINC - 0x01;
 		break;
 
-		case Keep_Inc:
+	case Keep_Inc:
 		break;
 
-		case Keep_Dec:
+	case Keep_Dec:
 		break;
 
-		case Fin_Inc:
+	case Fin_Inc:
 		break;
 
-		case Fin_Dec:
+	case Fin_Dec:
 		break;
 
-		case Reset:
+	case Reset:
 		break;
 
-		case Fin_Reset:
+	case Fin_Reset:
 		PORTC = 0x00;
 		break;
 
-		default:
+	default:
 		break;
 	} // State actions
-	
+
 }
 
 
@@ -142,11 +142,9 @@ int main(void)
 	DDRA = 0x00; PORTA = 0xFF;
 	DDRC = 0xFF; PORTC = 0x00;
 	States = Start;
-	
+
 	while (1)
 	{
 		tick();
 	}
 }
-
-
