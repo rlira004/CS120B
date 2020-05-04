81012e3bc2de9a92e1fd10252c6b473219ba381c
Ricardo <63069526+rlira004@users.noreply.github.com>
2020-05-03 19:51:38 -0700

    test

--- lab5/source/main.c
+++ lab5/source/main.c
@@ -1,53 +1,152 @@
 /*	Author: Ricardo Lira rlira004@ucr.edu
- *  Partner(s) Name: 
+ *  Partner(s) Name:
  *	Lab Section: 025
- *	Assignment: Lab #5  Exercise #1
+ *	Assignment: Lab#04  Exercise#02
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
  */
 #include <avr/io.h>
-#ifdef _SIMULATE_
-#include "simAVRHeader.h"
-#endif
 
-int main(void) {
-    /* Insert DDR and PORT initializations */
-    DDRA = 0x00; PORTA = 0xFF; 
-    DDRC = 0xFF; PORTC = 0x00;
-    unsigned char tmpA = 0x00;
-    /* Insert your solution below */
-    while (1) {
-        PORTC = 0x00;
-	tmpA = PINA;
-	if(tmpA == 0xFF) {
-		PORTC = 0x40;
+enum States {Start, Wait, Inc, Keep_Inc, Dec, Keep_Dec, Stop_Inc, Stop_Dec, Dbl_Press, Stop_Dbl_Press} States;
+unsigned char tempA = 0x00;
+
+void tick(){
+	tempA = ~PINA;
+	switch (States)
+	{
+		case Start:
+		States = Wait;
+		break;
+		
+		case Wait:
+		if ((tempA & 0x03) == 0x01)
+			States = Inc;
+		else if ((tempA & 0x03) == 0x02)
+			States = Dec;
+		else
+			States = Wait;
+		break;
+
+		case Inc:
+		if((tempA & 0x03) == 0x01)
+			States = Keep_Inc;
+		break;
+
+		case Dec:
+		if((tempA & 0x03) == 0x02)
+			States = Keep_Dec;
+		break;
+
+		case Keep_Inc:
+		if((tempA & 0x03) == 0x01)
+			States = Keep_Inc;
+		else if ((tempA & 0x03) == 0x00)
+			States = Stop_Inc;
+		else if((tempA & 0x03) == 0x03)
+			States = Dbl_Press;
+		break;
+
+		case Keep_Dec:
+		if((tempA & 0x03) == 0x02)
+			States = Keep_Dec;
+		else if ((tempA & 0x03) == 0x00)
+			States = Stop_Dec;
+		else if((tempA & 0x03) == 0x03)
+			States = Dbl_Press;
+		break;
+
+		case Stop_Inc:
+		if ((tempA & 0x03) == 0x00)
+			States = Stop_Inc;
+		else if ((tempA & 0x03) == 0x01)
+			States = Inc;
+		else if ((tempA & 0x03) == 0x02)
+			States = Dec;
+		break;
+
+		case Stop_Dec:
+		if ((tempA & 0x03) == 0x00)
+			States = Stop_Dec;
+		else if ((tempA & 0x03) == 0x02)
+			States = Dec;
+		else if ((tempA & 0x03) == 0x01)
+			States = Inc;
+		break;
+
+		case Dbl_Press:
+		if ((tempA & 0x03) == 0x11)
+			States = Dbl_Press;
+		else if ((tempA & 0x03) == 0x01)
+			States = Dbl_Press;
+		else if ((tempA & 0x03) == 0x10)
+			States = Dbl_Press;
+		else if ((tempA & 0x03) == 0x00)
+			States = Stop_Dbl_Press;
+		break;
+
+		case Stop_Dbl_Press:
+		States = Wait;
+		break;
+		
+		default:
+		break;
+		
+		
 	}
-	if(tmpA == 0x7C) {
-		PORTC = 0x70;
-	}
-	if(tmpA == 0xF8) {
-		PORTC = 0x3C;
-	}
-	if(tmpA == 0x01 || tmpA == 0x02) {
-		PORTC = 0x60;
-	}
-	if(tmpA > 0x02 && tmpA < 0x05) {
-                PORTC = 0x70;
-        }
-	if(tmpA > 0x04 && tmpA < 0x07) {
-                PORTC = 0x38;
-        }
-	if(tmpA > 0x06 && tmpA < 0x0A) {
-                PORTC = 0x3C;
-        }
-	if(tmpA > 0x09 && tmpA < 0x0D) {
-                PORTC = 0x3E;
-        }
-	if(tmpA > 0x0C && tmpA < 0x10) {
-                PORTC = 0x3F;
-        }	
-    }
-	return 1;
+	
+	switch(States) {   // State actions
+		case Wait:
+		PORTC = 0x00;
+		break;
+
+		case Inc:
+		if(PINC != 0x09)
+			PORTC = PINC + 0x01;
+		break;
+
+		case Dec:
+		if(PINC != 0x00)
+			PORTC = PINC - 0x01;
+		break;
+
+		case Keep_Inc:
+		break;
+
+		case Keep_Dec:
+		break;
+
+		case Stop_Inc:
+		break;
+
+		case Stop_Dec:
+		break;
+
+		case Dbl_Press:
+		break;
+
+		case Stop_Dbl_Press:
+		PORTC = 0x00;
+		break;
+
+		default:
+		break;
+	} // State actions
+	
 }
+
+
+int main(void)
+{
+	DDRA = 0x00; PORTA = 0xFF;
+	DDRC = 0xFF; PORTC = 0x00;
+	States = Start;
+	
+	while (1)
+	{
+		tick();
+	}
+}
+
+
