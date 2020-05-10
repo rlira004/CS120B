8082aa724f7c6152c8244504d0130ea724ca3c95
rlira004 <rlira004@ucr.edu>
2020-05-10 15:10:04 -0700

    Lab 6 1&2

--- lab6/turnin/rlira004_lab6_part1.c
+++ lab6/turnin/rlira004_lab6_part1.c
@@ -1,9 +1,9 @@
 /*	Author: Ricardo Lira rlira004@ucr.edu
  *  Partner(s) Name: 
  *	Lab Section:
- *	Assignment: Lab #06  Exercise #02
+ *	Assignment: Lab #06  Exercise #01
  *	Exercise Description: [optional - include for your own benefit]
- *      youtube.com/watch?v=Sx62MaYiE3E
+ *      youtube.com/watch?v=DxletANwxfY
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
  */
@@ -16,7 +16,7 @@
 unsigned char tmpB = 0x00;
 unsigned char tmpA = 0x00;
 unsigned char cnt = 0x00;
-enum States {start, one, two, three, retwo, hold, reset } state;
+enum States {start, one, two, three/*, retwo, hold*/ } state;
 
 void tick() {
 	tmpA = ~PINA;
@@ -25,45 +25,36 @@ void tick() {
 			state = one;
 			break;
 		case one:
-			if(tmpA == 0x00) {
-				state = two;
-			}
-			else if(tmpA != 0x00)
-			state = hold;
+		//	if(tmpA == 0x01) {
+		//		state = hold;
+		//	}
+		//	else
+			state = two;
 			break;
 		case two:
-			if(tmpA == 0x00) {
-				state = three;
-			}
-			else if(tmpA != 0x00)
-			state = hold;
+		//	if(tmpA == 0x01) {
+		//		state = hold;
+		//	}
+		//	else
+			state = three;
 			break;
 		case three:
-			if(tmpA == 0x00) {
-				state = retwo;
-			}
-			else if(tmpA != 0x00)
-			state = hold;
+		//	if(tmpA == 0x01) {
+		//		state = hold;
+		//	}
+		//	else
+		//	state = retwo;
+			state = one;
 			break;
-		case retwo:
-			if(tmpA == 0x00) {
-				state = one;
-			}
-			else if(tmpA != 0x00)
-			state = hold;
-			break;
-		case hold:
-			if(tmpA == 0x00)
-				state = reset;
-			else if (tmpA != 0x00)
-				state = hold;
-			break;
-		case reset:
-			if(tmpA == 0x00)
-				state = reset;
-			else if(tmpA == 0x01)
-				state = one;
-			break;		
+		//case retwo:
+		//	if(tmpA == 0x01) {
+		//		state = hold;
+		//	}
+		//	else
+		//	state = one;
+		//	break;
+		//case hold:
+		//	break;		
 		default:
 			state = start;
 			break;
@@ -81,13 +72,11 @@ void tick() {
 		case three:
 			tmpB = 0x04;
 			break;
-		case retwo:
-			tmpB = 0x02;
-			break;
-		case hold:
-			break;
-		case reset:
-			break;
+		//case retwo:
+		//	tmpB = 0x02;
+		//	break;
+		//case hold:
+		//	break;
 		default:
 			break;
 	}
@@ -99,7 +88,7 @@ int main(void) {
 	DDRA = 0x00; PORTA = 0xFF; 
 	DDRB = 0xFF; PORTB = 0x00;
         //Insert your solution below 
-	TimerSet (3000);
+	TimerSet (1000);
 	TimerOn();
 	state = start;
     while (1) {
