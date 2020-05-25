3ab5c05b355a15ca286b3312763be397695ffc30
Ricardo <63069526+rlira004@users.noreply.github.com>
2020-05-24 21:46:57 -0700

    update

--- lab10/source/main.c
+++ lab10/source/main.c
@@ -1,15 +1,15 @@
 /*	Author: Ricardo Lira rlira004@ucr.edu
  *  Partner(s) Name: 
  *	Lab Section: 025
- *	Assignment: Lab #08  Exercise #04
+ *	Assignment: Lab #10  Exercise #01
  *	Exercise Description: [optional - include for your own benefit]
- *      youtube.com/watch?v=	
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
  */
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
+#include "timer.h"
 
 unsigned char threeLEDs = 0x00;
 unsigned char blinkingLED = 0x00;
@@ -99,32 +99,6 @@ void TickFct_Combine_StateSM(){
 volatile unsigned char TimerFlag = 0;
 unsigned long _avr_timer_M = 1;
 unsigned long _avr_timer_cntcurr = 0;
-
-void TimerOn(){
-	TCCR1B = 0x0B;
-	OCR1A = 125;
-	TIMSK1 = 0x02;
-	TCNT1 = 0;
-	_avr_timer_cntcurr = _avr_timer_M;
-	SREG |= 0x80;
-}
-void TimerOff(){
-	TCCR1B = 0x00;
-}
-void TimerISR(){
-	TimerFlag = 1;
-}
-ISR(TIMER1_COMPA_vect){
-	_avr_timer_cntcurr--;
-	if(_avr_timer_cntcurr == 0){
-		TimerISR();
-		_avr_timer_cntcurr = _avr_timer_M;
-	}
-}
-void TimerSet(unsigned long M){
-	_avr_timer_M = M;
-	_avr_timer_cntcurr = _avr_timer_M;
-}
 
 int main(void)
 {
