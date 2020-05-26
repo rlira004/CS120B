480307cb67c2a4f971530cee8923728d6d8e90e9
Ricardo <63069526+rlira004@users.noreply.github.com>
2020-05-26 00:26:20 -0700

    given test

--- Midterm2/source/main.c
+++ Midterm2/source/main.c
@@ -1,171 +1,52 @@
 /*	Author: Ricardo Lira rlira004@ucr.edu
  *  Partner(s) Name: 
  *	Lab Section: 025
- *	Assignment: Lab #10  Exercise #01
+ *	Assignment: Midterm2  Exercise #00
  *	Exercise Description: [optional - include for your own benefit]
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
  */
 
 #include <avr/io.h>
-#include <avr/interrupt.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
 #include "timer.h"
 
-unsigned char threeLEDs = 0x00;
-unsigned char blinkingLED = 0x00;
+/* SM state declarations --- fill in as needed */
+typedef enum output_states { OInit, Ooutput } output_states;
+typedef enum read_states { RInit, Rread } read_states;
 
-enum threeLEDsSm { threeLED_Start, light1, light2, light3 } threeLEDs_States;
-enum blinkingLEDSM { blinkingLED_Start, blinking_off, blinking_on } blinkingLED_States;
-enum CombineLEDsSM { Combine_start } combine_State;
-enum SpeakerStates {init, on, off, hold} Speaker_State;
+/* shared variables --- fill in as needed */
+unsigned char PA;
 
-#define tmpA (~PINA & 0x04)
-void tick_Speaker() {
-	 switch(Speaker_State) {
-		case init:
-			Speaker_State = off;
-			break;
-		case off:
-			if (tmpA == 0x04) {
-				Speaker_State = on;
-			}
-			else if (tmpA == 0x00) {
-				Speaker_State = off;
-			}
-			break;
-		case on:
-			if(tmpA == 0x00) {
-				Speaker_State = off;
-			}
-			else if (tmpA == 0x04) {
-				Speaker_State = hold;
-			}
-			break;
-		case hold:
-			if(tmpA == 0x00) {
-				Speaker_State = off;
-			}
-			else if (tmpA == 0x04) {
-				Speaker_State = on;
-			}
-			break;
-		default:
-			Speaker_State = init;
-			break;
-}
-void tick_threeLEDsSM() {
-	switch (threeLEDs_States) {
-	case threeLED_Start:
-		threeLEDs_States = light1;
-		break;
-	case light1:
-		threeLEDs_States = light2;
-		break;
-	case light2:
-		threeLEDs_States = light3;
-		break;
-	case light3:
-		threeLEDs_States = light1;
-		break;
-	default:
-		threeLEDs_States = threeLED_Start;
-		break;
-	}
-	switch (threeLEDs_States) {
-	case threeLED_Start:
-		break;
-	case light1:
-		threeLEDs = 0x01;
-		break;
-	case light2:
-		threeLEDs = 0x02;
-		break;
-	case light3:
-		threeLEDs = 0x04;
-		break;
-	default:
-		break;
-	}
-}
+/* state variables --- do not alter */
+output_states output_state;
+read_states read_state;
 
-void tick_blinkingLEDSM() {
-	switch (blinkingLED_States)
-	{
-	case blinkingLED_Start:
-		blinkingLED_States = blinking_off;
-		break;
-	case blinking_off:
-		blinkingLED_States = blinking_on;
-		break;
-	case blinking_on:
-		blinkingLED_States = blinking_off;
-		break;
-	default:
-		break;
-	}
-	switch (blinkingLED_States)
-	{
-	case blinkingLED_Start:
-		break;
-	case blinking_off:
-		blinkingLED = 0x00;
-		break;
-	case blinking_on:
-		blinkingLED = 0x08;
-		break;
-	default:
-		break;
-	}
-}
-void tick_Combine_State() {
-	switch (combine_State) {
-	case Combine_start:
-		break;
-	}
-	switch (combine_State) {
-	case Combine_start:
-		PORTB = threeLEDs | blinkingLED;
-		break;
-	default:
-		break;
-	}
-}
+/* SM definitions --- complete each task as a SM */
+#include "output.h"
+#include "read.h"
 
-int main(void){
-	unsigned long threeLED_StateTimer = 0;
-	unsigned long blinkingLED_StateTimer = 0;
-	unsigned long Period = 1;
-	unsigned long speaker_StateTimer = 0;
+/* main function --- do not alter */
+int main(void) {
+    /* DDR and PORT initializations */
+    DDRA = 0x00; PORTA = 0xFF;
+    DDRB = 0xFF; PORTB = 0x00;
 
+    // Common period for all tasks set to 100ms
+    TimerSet(100);
+    TimerOn();
 
-	TimerSet(Period);
-	TimerOn();
+    // init state vars
+    output_state = OInit;
+    read_state = RInit;
 
-	threeLEDs_States = threeLED_Start;
-	blinkingLED_States = blinkingLED_Start;
-	combine_State = Combine_start;
-	Speaker_State = init;
-	while (1)
-	{
-		if (speaker_StateTimer >= 2) {
-			tick_Speaker();
-			speaker_StateTimer = 0;
-		}
-		if (threeLED_StateTimer >= 300) {
-			tick_threeLEDsSM();
-			threeLED_StateTimer = 0;
-		}
-		if (blinkingLED_StateTimer >= 1000) {
-			tick_blinkingLEDSM();
-			blinkingLED_StateTimer = 0;
-		}
-		tick_Combine_State();
-
-		while (!TimerFlag) {}
-		TimerFlag = 0;
-
-		threeLED_StateTimer += Period;
-		blinkingLED_StateTimer += Period;
-		speaker_StateTimer += Period;
-	}
+    while (1) {
+        Read();
+        Output();
+        while (!TimerFlag) { }
+        TimerFlag = 0;
+    }
+    return 1;
 }
