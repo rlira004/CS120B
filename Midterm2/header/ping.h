5dd9f4c171c13592ec20dc3e25bbeb178fea2a2d
Ricardo <63069526+rlira004@users.noreply.github.com>
2020-05-27 13:05:19 -0700

    Update ping.h

--- Midterm2/header/ping.h
+++ Midterm2/header/ping.h
@@ -23,7 +23,7 @@ void Ping()
         case PInit:
                //cnt ++;
                // if (cnt > 3) {
-                    PORTB = (~PORTB & 0x00);
+                    PORTB = (~PORTB & 0x01);
                 //    cnt = 0;
                // }
                 //else
