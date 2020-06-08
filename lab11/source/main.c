73c657cdc6cb345f5bf6fbe05795449454717606
Ricardo <63069526+rlira004@users.noreply.github.com>
2020-06-07 22:35:43 -0700

    Update main.c

--- lab11/source/main.c
+++ lab11/source/main.c
@@ -8,164 +8,118 @@
  */
 
 #include <avr/io.h>
-#include <avr/interrupt.h>
+#include "keypad.h"
+#include "scheduler.h"
 #include "timer.h"
+#include "bit.h"
 
-unsigned char threeLEDs = 0x00;
-unsigned char blinkingLED = 0x00;
+unsigned char tmpB = 0x00;
 
-enum threeLEDsSm { threeLED_Start, light1, light2, light3 } threeLEDs_States;
-enum blinkingLEDSM { blinkingLED_Start, blinking_off, blinking_on } blinkingLED_States;
-enum CombineLEDsSM { Combine_start } combine_State;
-enum SpeakerStates {init, on, off, hold} Speaker_State;
+enum SM_Task {SM_output};
 
-#define tmpA (~PINA & 0x04)
-void tick_Speaker() {
-	 switch(Speaker_State) {
-		case init:
-			Speaker_State = off;
+int SM_TaskTick(int state){
+	unsigned char x = 0;
+	x = GetKeypadKey();
+	
+	switch(state){
+		case SM_output:
+			switch(x){
+				case '\0':
+					tmpB = 0x1F;
+					break;
+				case '1':
+					tmpB = 0x01;
+					break;
+				case '2':
+					tmpB = 0x02;
+					break;
+				case '3':
+					tmpB = 0x03;
+					break;
+				case '4':
+					tmpB = 0x04;
+					break;
+				case '5':
+					tmpB = 0x05;
+					break;
+				case '6':
+					tmpB = 0x06;
+					break;
+				case '7':
+					tmpB = 0x07;
+					break;
+				case '8':
+					tmpB = 0x08;
+					break;
+				case '9':
+					tmpB = 0x09;
+					break;
+				case 'A':
+					tmpB = 0x0A;
+					break;
+				case 'B':
+					tmpB = 0x0B;
+					break;
+				case 'C':
+					tmpB = 0x0C;
+					break;
+				case 'D':
+					tmpB = 0x0D;
+					break;
+				case '*':
+					tmpB = 0x0E;
+					break;
+				case '0':
+					tmpB = 0x00;
+					break;
+				case '#':
+					tmpB = 0x0F;
+					break;
+				default:
+					tmpB = 0x1B;
+					break;
+			}
+			state = SM_output;
+			PORTB = tmpB;
 			break;
-		case off:
-			if (tmpA == 0x04) {
-				Speaker_State = on;
-			}
-			else if (tmpA == 0x00) {
-				Speaker_State = off;
-			}
-			break;
-		case on:
-			if(tmpA == 0x00) {
-				Speaker_State = off;
-			}
-			else if (tmpA == 0x04) {
-				Speaker_State = hold;
-			}
-			break;
-		case hold:
-			if(tmpA == 0x00) {
-				Speaker_State = off;
-			}
-			else if (tmpA == 0x04) {
-				Speaker_State = on;
-			}
-			break;
-		default:
-			Speaker_State = init;
-			break;
-}
-void tick_threeLEDsSM() {
-	switch (threeLEDs_States) {
-	case threeLED_Start:
-		threeLEDs_States = light1;
-		break;
-	case light1:
-		threeLEDs_States = light2;
-		break;
-	case light2:
-		threeLEDs_States = light3;
-		break;
-	case light3:
-		threeLEDs_States = light1;
-		break;
-	default:
-		threeLEDs_States = threeLED_Start;
-		break;
 	}
-	switch (threeLEDs_States) {
-	case threeLED_Start:
-		break;
-	case light1:
-		threeLEDs = 0x01;
-		break;
-	case light2:
-		threeLEDs = 0x02;
-		break;
-	case light3:
-		threeLEDs = 0x04;
-		break;
-	default:
-		break;
-	}
+	return state;
 }
 
-void tick_blinkingLEDSM() {
-	switch (blinkingLED_States)
-	{
-	case blinkingLED_Start:
-		blinkingLED_States = blinking_off;
-		break;
-	case blinking_off:
-		blinkingLED_States = blinking_on;
-		break;
-	case blinking_on:
-		blinkingLED_States = blinking_off;
-		break;
-	default:
-		break;
-	}
-	switch (blinkingLED_States)
-	{
-	case blinkingLED_Start:
-		break;
-	case blinking_off:
-		blinkingLED = 0x00;
-		break;
-	case blinking_on:
-		blinkingLED = 0x08;
-		break;
-	default:
-		break;
-	}
-}
-void tick_Combine_State() {
-	switch (combine_State) {
-	case Combine_start:
-		break;
-	}
-	switch (combine_State) {
-	case Combine_start:
-		PORTB = threeLEDs | blinkingLED;
-		break;
-	default:
-		break;
-	}
-}
+int main(void)
+{
+    DDRA = 0xF0; PORTA = 0x0F; 
+	DDRB = 0xFF; PORTB = 0x00;
+	
+	unsigned long int SM_TaskTick_time = 20;
+	unsigned long int tmpGCD = 10;
+	unsigned long int GCD = tmpGCD;
+	unsigned long int SM_Task_period = SM_TaskTick_time/GCD;
+	
+	static task task1;
+	task *tasks[] = {&task1};
+	const unsigned short numTasks = sizeof(tasks)/sizeof(task*);
 
-int main(void){
-	unsigned long threeLED_StateTimer = 0;
-	unsigned long blinkingLED_StateTimer = 0;
-	unsigned long Period = 1;
-	unsigned long speaker_StateTimer = 0;
-
-
-	TimerSet(Period);
+	task1.state = 0;
+	task1.period = SM_Task_period;
+	task1.elapsedTime = SM_Task_period;
+	task1.TickFct = &SM_TaskTick;
+	
+	TimerSet(GCD);
 	TimerOn();
-
-	threeLEDs_States = threeLED_Start;
-	blinkingLED_States = blinkingLED_Start;
-	combine_State = Combine_start;
-	Speaker_State = init;
-	while (1)
-	{
-		if (speaker_StateTimer >= 2) {
-			tick_Speaker();
-			speaker_StateTimer = 0;
+	
+	unsigned short i;
+    while (1) {
+		for (i = 0; i < numTasks; i++){
+			if (tasks[i] ->elapsedTime == tasks[i] ->period ){
+				tasks[i] ->state = tasks[i] ->TickFct(tasks[i] ->state);
+				tasks[i] ->elapsedTime = 0;
+			}
+			tasks[i] ->elapsedTime += 1;
 		}
-		if (threeLED_StateTimer >= 300) {
-			tick_threeLEDsSM();
-			threeLED_StateTimer = 0;
-		}
-		if (blinkingLED_StateTimer >= 1000) {
-			tick_blinkingLEDSM();
-			blinkingLED_StateTimer = 0;
-		}
-		tick_Combine_State();
-
-		while (!TimerFlag) {}
+		
+		while(!TimerFlag);
 		TimerFlag = 0;
-
-		threeLED_StateTimer += Period;
-		blinkingLED_StateTimer += Period;
-		speaker_StateTimer += Period;
-	}
+  }
+	return 0;
 }
